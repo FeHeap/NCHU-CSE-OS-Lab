2ba17fc07dc180aa6258bf4d7bf3103a55695f85
FeHeap <maybeicannot@gmail.com>
2021-10-09 23:03:01 +0800

    rm some file

--- Lab05/HelloModule_v2/.hello_module.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/fe/Desktop/NCHU-CSE-OS-Lab/Lab05/HelloModule_v2/hello_module.mod := { echo  /home/fe/Desktop/NCHU-CSE-OS-Lab/Lab05/HelloModule_v2/hello_module.o; echo; } > /home/fe/Desktop/NCHU-CSE-OS-Lab/Lab05/HelloModule_v2/hello_module.mod
